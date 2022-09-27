f21561401ed0ca1de537cb161660b433ffd44824
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-09-27 11:03:34 +0900

    Update 자릿수더하기.java

--- Assignment/자릿수더하기.java
+++ Assignment/자릿수더하기.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Firstassignment {
-    public Firstassignment() {
-    }
-
-    public static void main(String[] args) {
+public class Firstassignment{
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println("0-999 사이의 숫자를 입력하세요: ");
+        //문자 입력을 String으로 받기
         String input = in.nextLine();
-        int sum = 0;
-
-        for(int i = 0; i < input.length(); ++i) {
-            sum += Integer.parseInt(input.substring(i, i + 1));
+        //sum 변수 설정 및 초기화
+        int sum=0;
+        //for문을 자릿수 숫자만큼 돌리고 각 자릿수의 합을 sum에 더하기
+        for(int i=0; i<input.length(); i++){
+            sum+= Integer.parseInt(input.substring(i,i+1));
         }
-
-        System.out.println("각 자리 수의 합=" + sum);
+        //값 출력하기
+        System.out.println("각 자리 수의 합="+sum);
     }
 }
