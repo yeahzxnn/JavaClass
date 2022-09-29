0a54e64be3938df00e7478f8f8cb97a4184fc659
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-09-29 10:48:56 +0900

    Create ColoredCirlce.java

---
+++ Assignment/ColoredCirlce.java
@@ -0,0 +1,32 @@
+package Assignment;
+
+class Circle{
+    int radius;
+    Circle(int radius){
+        this.radius = radius;
+    }
+    void show(){
+        System.out.println("반지름이"+radius+"인 원이다.");
+    }
+}
+class ColoredCircle extends Circle{
+    String color;
+    
+    ColoredCircle(int radius, String color){
+        super(radius);
+        this.color = color;
+    }
+    void show(){
+        System.out.println("반지름이"+radius+"인"+color+"원이다.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Circle c1 = new Circle(5);
+        ColoredCircle c2 = new ColoredCircle(10,"빨간색");
+        
+        c1.show();
+        c2.show();
+    }
+}
