009c9b5063b6cd606a07735d48072451b20cae8e
yeahzxnn <102504021+yeahzxnn@users.noreply.github.com>
2022-10-18 12:35:19 +0900

    Create showTokens.java

---
+++ Assignment/showTokens.java
@@ -0,0 +1,23 @@
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+public class TokenPrintTest {
+
+    public static void main(String[] args) {
+
+        String s = "of the people, by the people, for the people";
+        try {
+            showTokens(s, ", ");
+        } catch (NoSuchElementException e) {
+            System.out.println("끝");
+        }
+    }
+    static void showTokens(String s, String s1){
+        StringTokenizer st = new StringTokenizer(s, s1);
+        while(true){
+            System.out.println(st.nextToken());
+        }
+    }
+}
+
+//Realsb...에게 무한감사....ㅠ
